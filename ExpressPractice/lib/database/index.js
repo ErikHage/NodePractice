b975f4ba5838378f3710703757dc20885a8455c7
Erik Hage <ehage4@gmail.com>
2017-11-19 21:09:40 -0500

    more tests

--- ExpressPractice/lib/database/index.js
+++ ExpressPractice/lib/database/index.js
@@ -10,88 +10,85 @@ const getOrAddCollection = (collectionName) => {
     ? db.addCollection(collectionName) : db.getCollection(collectionName);
 };
 
-const insert = (obj, collectionName) => {
-  let result;
+class Database {
+  static insert(obj, collectionName) {
+    let result;
 
-  const collection = getOrAddCollection(collectionName);
+    const collection = getOrAddCollection(collectionName);
 
-  try {
-    result = collection.insert(obj);
-  } catch (err) {
-    return Promise.reject(new Error(`Error inserting to ${collectionName}`, err));
-  }
+    try {
+      result = collection.insert(obj);
+    } catch (err) {
+      return Promise.reject(new Error(`Error inserting to ${collectionName}`, err));
+    }
 
-  if(result !== null) {
-    return Promise.resolve(result);
-  } else {
-    return Promise.reject(new Error(`Insert to ${collectionName} failed with unknown error`));
-  }
-};
+    if (result !== null) {
+      return Promise.resolve(result);
+    } else {
+      return Promise.reject(new Error(`Insert to ${collectionName} failed with unknown error`));
+    }
+  };
 
-const findOne = (criteria, collectionName) => {
-  let result;
+  static findOne(criteria, collectionName) {
+    let result;
 
-  const collection = getOrAddCollection(collectionName);
+    const collection = getOrAddCollection(collectionName);
 
-  try {
-    result = collection.findOne(criteria);
-  } catch (err) {
-    return Promise.reject(new Error(`Error findOne from ${collectionName}`, err));
-  }
+    try {
+      result = collection.findOne(criteria);
+    } catch (err) {
+      return Promise.reject(new Error(`Error findOne from ${collectionName}`, err));
+    }
 
-  if(result !== null) {
-    return Promise.resolve(result);
-  } else {
-    return Promise.reject(new Error(`FindOne from ${collectionName} failed with unknown error`));
-  }
-};
+    if (result !== null) {
+      return Promise.resolve(result);
+    } else {
+      return Promise.reject(new Error(`FindOne from ${collectionName} failed with unknown error`));
+    }
+  };
 
-const update = (criteria, obj, collectionName) => {
-  let result;
+  static update(criteria, obj, collectionName) {
+    let result;
 
-  const collection = getOrAddCollection(collectionName);
+    const collection = getOrAddCollection(collectionName);
 
-  try {
-    collection.findAndUpdate(criteria, (data) => {
-      for(let key in obj) {
-        if(obj.hasOwnProperty(key)) {
-          data[key] = obj[key];
+    try {
+      collection.findAndUpdate(criteria, (data) => {
+        for (let key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            data[key] = obj[key];
+          }
         }
-      }
-    });
-    result = collection.findOne(criteria);
-  } catch (err) {
-    return Promise.reject(new Error(`Error updating to ${collectionName}`, err));
-  }
+      });
+      result = collection.findOne(criteria);
+    } catch (err) {
+      return Promise.reject(new Error(`Error updating to ${collectionName}`, err));
+    }
 
-  if(result !== null) {
-    return Promise.resolve(result);
-  } else {
-    return Promise.reject(new Error(`Update to ${collectionName} failed with unknown error`));
-  }
-};
+    if (result !== null) {
+      return Promise.resolve(result);
+    } else {
+      return Promise.reject(new Error(`Update to ${collectionName} failed with unknown error`));
+    }
+  };
 
-const remove = (criteria, collectionName) => {
-  const collection = getOrAddCollection(collectionName);
+  static remove(criteria, collectionName) {
+    const collection = getOrAddCollection(collectionName);
 
-  try {
-    collection.findAndRemove(criteria);
-  } catch (err) {
-    return Promise.reject(new Error(`Error removing from ${collectionName}`, err));
-  }
+    try {
+      collection.findAndRemove(criteria);
+    } catch (err) {
+      return Promise.reject(new Error(`Error removing from ${collectionName}`, err));
+    }
 
-  let remaining = collection.find(criteria);
+    let remaining = collection.find(criteria);
 
-  if(remaining.length > 0) {
-    return Promise.reject(new Error(`Remove from ${collectionName} failed with unknown error`));
-  } else {
-    return Promise.resolve();
-  }
-};
+    if (remaining.length > 0) {
+      return Promise.reject(new Error(`Remove from ${collectionName} failed with unknown error`));
+    } else {
+      return Promise.resolve();
+    }
+  };
+}
 
-module.exports = {
-  insert,
-  findOne,
-  update,
-  remove,
-};
+module.exports = Database;
--- ExpressPractice/spec/unit/datasource/beer/index.spec.js
+++ ExpressPractice/spec/unit/datasource/beer/index.spec.js
@@ -9,32 +9,67 @@ const Database = require('../../../../lib/database');
 
 describe('BeerDataSource', () => {
   let databaseMock;
+  let beer;
+  let dbResult;
 
-  const beer = {
-    beerId: 'Some UUID',
-    name: 'Some Beer',
-    style: 'Some Style',
-    abv: 5.5,
-    ibu: 35.5,
-    description: 'Some description',
-  };
+  const collectionName = 'beers';
 
   beforeEach(() => {
     this.sandbox = sinon.sandbox.create();
     databaseMock = sinon.mock(Database);
+
+    beer = {
+      beerId: 'Some UUID',
+      name: 'Some Beer',
+      style: 'Some Style',
+      abv: 5.5,
+      ibu: 35.5,
+      description: 'Some description',
+    };
+
+    dbResult = {
+      beerId: 'Some UUID',
+      name: 'Some Beer',
+      style: 'Some Style',
+      abv: 5.5,
+      ibu: 35.5,
+      description: 'Some description',
+      meta: { revision: 0, created: 1511134154296, version: 0 },
+      $loki: 1
+    };
   });
 
   afterEach(() => {
     databaseMock.verify();
     this.sandbox.restore();
+
+    beer = null;
+    dbResult = null;
   });
 
   context('#insertBeer', () => {
     it('should successfully insert and return the inserted beer', (done) => {
-      done();
+      databaseMock.expects('insert')
+        .once()
+        .withArgs(beer, collectionName)
+        .returns(Promise.resolve(dbResult));
+
+      BeerDataSource.insertBeer(beer)
+        .then((res) => {
+          expect(res).to.deep.equal(beer);
+          done();
+        })
+        .catch(done);
     });
 
     it('should throw an error when save fails', (done) => {
+      const expectedError = new Error('Something went wrong!');
+
+      databaseMock.expects('insert')
+        .once()
+        .withArgs(beer, collectionName)
+        .returns(Promise.reject(expectedError));
+
       BeerDataSource.insertBeer(beer)
         .then(() => done(new Error('Expected error but not thrown')))
         .catch(() => done());
@@ -43,65 +78,85 @@ describe('BeerDataSource', () => {
 
   context('#getBeerById', () => {
     it('should successfully return the beer with the given id', (done) => {
-      const beerId = 'Some UUID';
-      const expected = {
-        beerId: 'Some UUID',
-        name: 'Some Beer',
-        style: 'Some Style',
-        abv: 5.5,
-        ibu: 35.5,
-        description: 'Some description',
-      };
+      databaseMock.expects('findOne')
+        .once()
+        .withArgs({ beerId: beer.beerId }, collectionName)
+        .returns(Promise.resolve(dbResult));
 
-      BeerDataSource.getBeerById(beerId)
+      BeerDataSource.getBeerById(beer.beerId)
         .then((res) => {
-          expect(res).to.deep.equal(expected);
+          expect(res).to.deep.equal(beer);
           done();
         })
         .catch(done);
     });
 
-    it('should throw an error when read fails');
+    it('should throw an error when read fails', (done) => {
+      const expectedError = new Error('Something went wrong!');
+
+      databaseMock.expects('findOne')
+        .once()
+        .withArgs({ beerId: beer.beerId }, collectionName)
+        .returns(Promise.reject(expectedError));
+
+      BeerDataSource.getBeerById(beer.beerId)
+        .then(() => done(new Error('Expected error but not thrown')))
+        .catch(() => done());
+    });
   });
 
   context('#updateBeer', () => {
     it('should successfully update the given beer', (done) => {
-      const beerToUpdate = {
-        beerId: 'Some UUID',
-        name: 'Some Beer Name',
-        style: 'Some Beer Style',
-        abv: 10.2,
-        ibu: 45.5,
-        description: 'Some updated description',
-      };
+      databaseMock.expects('update')
+        .once()
+        .withArgs({ beerId: beer.beerId }, beer, collectionName)
+        .returns(Promise.resolve(dbResult));
 
-      const expected = {
-        beerId: 'Some UUID',
-        name: 'Some Beer Name',
-        style: 'Some Beer Style',
-        abv: 10.2,
-        ibu: 45.5,
-        description: 'Some updated description',
-      };
-
-      BeerDataSource.updateBeer(beerToUpdate)
+      BeerDataSource.updateBeer(beer)
         .then((res) => {
-          expect(res).to.deep.equal(expected);
+          expect(res).to.deep.equal(beer);
           done();
         })
         .catch(done);
     });
 
-    it('should throw an error when update fails');
+    it('should throw an error when update fails', (done) => {
+      const expectedError = new Error('Something went wrong!');
+
+      databaseMock.expects('update')
+        .once()
+        .withArgs({ beerId: beer.beerId }, beer, collectionName)
+        .returns(Promise.reject(expectedError));
+
+      BeerDataSource.updateBeer(beer)
+        .then(() => done(new Error('Expected error but not thrown')))
+        .catch(() => done());
+    });
   });
 
   context('#deleteBeer', () => {
     it('should successfully delete beer for given id', (done) => {
+      databaseMock.expects('remove')
+        .once()
+        .withArgs({ beerId: beer.beerId }, collectionName)
+        .returns(Promise.resolve());
+
       BeerDataSource.deleteBeer(beer.beerId)
         .then(() => done())
         .catch(done);
     });
 
-    it('should throw an error when delete fails');
+    it('should throw an error when delete fails', (done) => {
+      const expectedError = new Error('Something went wrong!');
+
+      databaseMock.expects('remove')
+        .once()
+        .withArgs({ beerId: beer.beerId }, collectionName)
+        .returns(Promise.reject(expectedError));
+
+      BeerDataSource.deleteBeer(beer.beerId)
+        .then(() => done(new Error('Expected error but not thrown')))
+        .catch(() => done());
+    });
   });
 });
